2d0b1411ebfb7dd75a69e5942c54cc3e2fb8207b
Viktor Biletskyi <106250265+f0remangod@users.noreply.github.com>
2023-01-22 13:48:59 +0200

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -27,8 +27,8 @@ export const App = () => {
             <Route path="/movies/" element={<Movies />} />
 
             <Route path="/movies/:movieId" element={<MovieDetails />}>
-              <Route path="/movies/:movieId/cast" element={<Cast />} />
-              <Route path="/movies/:movieId/reviews" element={<Reviews />} />
+              <Route path="cast" element={<Cast />} />
+              <Route path="reviews" element={<Reviews />} />
             </Route>
           </Routes>
         </Suspense>
